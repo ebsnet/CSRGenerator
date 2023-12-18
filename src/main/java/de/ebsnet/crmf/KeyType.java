a91fa82356fca71138bf50bf04bc5207a5882951
Valentin Brandl <valentin.brandl@ebsnet.de>
2023-12-18 09:59:13 +0100

    Initial commit

---
+++ src/main/java/de/ebsnet/crmf/KeyType.java
@@ -0,0 +1,38 @@
+package de.ebsnet.crmf;
+
+import java.util.Optional;
+import org.bouncycastle.asn1.x509.ExtendedKeyUsage;
+import org.bouncycastle.asn1.x509.KeyPurposeId;
+import org.bouncycastle.asn1.x509.KeyUsage;
+
+enum KeyType {
+  SIG,
+  ENC,
+  TLS;
+
+  public KeyUsage keyUsage() {
+    switch (this) {
+      case ENC:
+        return new KeyUsage(KeyUsage.keyAgreement | KeyUsage.keyEncipherment);
+      case SIG:
+      case TLS:
+        return new KeyUsage(KeyUsage.digitalSignature);
+      default:
+        throw new UnsupportedOperationException("unreachable... switched on " + this);
+    }
+  }
+
+  public Optional<ExtendedKeyUsage> extendedKeyUsage() {
+    switch (this) {
+      case TLS:
+        return Optional.of(
+            new ExtendedKeyUsage(
+                new KeyPurposeId[] {KeyPurposeId.id_kp_serverAuth, KeyPurposeId.id_kp_clientAuth}));
+      case ENC:
+      case SIG:
+        return Optional.empty();
+      default:
+        throw new UnsupportedOperationException("unreachable... switched on " + this);
+    }
+  }
+}
