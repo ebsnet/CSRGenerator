01772d68b4bfa375c3d3379faf74ebf3acec6c50
Valentin Brandl <valentin.brandl@ebsnet.de>
2024-02-27 09:35:26 +0100

    Make KeyType public

--- src/main/java/de/ebsnet/crmf/KeyType.java
+++ src/main/java/de/ebsnet/crmf/KeyType.java
@@ -5,7 +5,7 @@ import org.bouncycastle.asn1.x509.ExtendedKeyUsage;
 import org.bouncycastle.asn1.x509.KeyPurposeId;
 import org.bouncycastle.asn1.x509.KeyUsage;
 
-enum KeyType {
+public enum KeyType {
   SIG,
   ENC,
   TLS;
