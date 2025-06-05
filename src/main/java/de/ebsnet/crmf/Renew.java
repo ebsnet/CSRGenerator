a2685aa2eae046a73fb7cca2bbbe8529d630eda4
Valentin Brandl <valentin.brandl@ebsnet.de>
2025-06-05 11:03:58 +0200

    Revert "Test PMD fail"
    This reverts commit 60f748b7e1dd2a11a66adba9f99c7a995ce61722.

--- src/main/java/de/ebsnet/crmf/Renew.java
+++ src/main/java/de/ebsnet/crmf/Renew.java
@@ -78,7 +78,7 @@ public final class Renew extends BaseCommand implements Callable<Void> {
 
   private Path trustChain;
 
-  public static void main(String[] args)
+  public static void main(final String[] args)
       throws InvalidNameException,
           CRMFException,
           GeneralSecurityException,
